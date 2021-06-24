5e95b09e17d2a7294ea32ae4ce5c089c2963ae04
mimin5 <you@example.com>
2021-06-24 13:29:27 +0700

    created user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('nama');
+            $table->string('username');
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
+
+
+        
         });
     }
 
@@ -32,5 +34,6 @@ class CreateUsersTable extends Migration
     public function down()
     {
         Schema::dropIfExists('users');
+
     }
 }
