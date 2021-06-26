ca0bf31616049bfb2de0a2c6794b6a568d116011
mimin5 <you@example.com>
2021-06-26 15:55:29 +0700

    created insert data function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Validator ;
+use Hash;
+use Session;
+use App\Model\User;
 
 class AuthController extends Controller
 {
@@ -11,4 +15,42 @@ class AuthController extends Controller
         return view('auth');
     }
 
+    public function register()
+    {
+        return view('auth.register');
+    }
+    public function register_p(Request $request)
+    {
+        $rules =[
+            'nama'=> 'required',
+            'username' => 'required',
+            'password' => 'required'
+        ];
+
+        $messages =[
+            'nama.required' => 'Nama Tidak Boleh Kosong',
+            'username.required' => 'Username Tidak Boleh Kosong',
+            'password.required' => 'Password Tidak Boleh Kosong!!!' 
+        ];
+
+        $validator = Validator::make($request->all(),$rules,$messages);
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput($request->all);
+            
+        }
+        $user = new User;
+        $user->nama = ($request->nama);
+        $user->username = ($request->username); 
+        $user->password = Hash::make($request->password);
+        $simpan = $user->save();
+        
+        if ($simpan){
+            Session::flash('success','Register berhasil! silahkan login untuk mengakses data');
+            return redirect()->route('login');
+        }
+        else{
+            Session::flash('errors','Register Gagal!!, silahlan ulangi lagi');
+            return redirect()->route('login');
+        }
+    }
 }
