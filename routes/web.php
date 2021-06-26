b158fcfd44924d5cb1ada03951d1cec591757671
mimin5 <you@example.com>
2021-06-26 14:08:37 +0700

    add /

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
     route::get('/', 'AuthController@index')->name('login');
-    route::post('register','AuthController@register');
+    route::post('/register','AuthController@register');
 });
