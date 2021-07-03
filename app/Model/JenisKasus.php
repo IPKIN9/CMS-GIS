1f2a46e92013911be102a72452feef7cd1eaa3b8
mimin5 <you@example.com>
2021-07-03 18:13:49 +0800

    fix

--- app/Model/JenisKasus.php
+++ app/Model/JenisKasus.php
@@ -7,12 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class JenisKasus extends Model
 {
     protected $table = 'jenis_kasus';
-    protected $allowedFields =['j_kasus'];
-    protected $useSoftDeletes = true;
-    protected $returnType = 'array';
-    protected $useTimestamps = true;
-    protected $createdField = 'created_at';
-    protected $updatedField = 'updated_at';
-    protected $deletedField = 'deleted_at';
-    
+    protected $allowedFields =['id','j_kasus','created_at','updated_at','deleted_at'];
 }
