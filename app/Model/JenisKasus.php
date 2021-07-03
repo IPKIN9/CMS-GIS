0e4cf91fad15281f1fb07cc458652af697cda988
mimin5 <you@example.com>
2021-07-03 15:41:45 +0800

    add timetamps

--- app/Model/JenisKasus.php
+++ app/Model/JenisKasus.php
@@ -8,4 +8,11 @@ class JenisKasus extends Model
 {
     protected $table = 'jenis_kasus';
     protected $allowedFields =['j_kasus'];
+    protected $useSoftDeletes = true;
+    protected $returnType = 'array';
+    protected $useTimestamps = true;
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+    protected $deletedField = 'deleted_at';
+    
 }
