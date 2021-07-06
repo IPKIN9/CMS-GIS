4458b29de952857aff33b2dabd01bc69bae5298a
mimin5 <you@example.com>
2021-07-06 15:06:38 +0800

    perbaikan file MAS-19

--- app/Model/JenisKasus.php
+++ app/Model/JenisKasus.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class JenisKasus extends Model
 {
     protected $table = 'jenis_kasus';
-    protected $allowedFields =['id','j_kasus','created_at','updated_at','deleted_at'];
+    protected $fillable =['id','j_kasus','created_at','updated_at','deleted_at'];
 }
