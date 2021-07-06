56f5c8b8387b7cd3f18428ce097c9c1ea3b26cd0
mimin5 <you@example.com>
2021-07-06 18:41:43 +0800

    create insert function with success notif

--- resources/views/admin/JenisKasus.blade.php
+++ resources/views/admin/JenisKasus.blade.php
@@ -83,7 +83,7 @@
                 <h4 class="modal-title">JENIS KASUS</h4>
             </div>
             <!-- body modal -->
-            <form action="" method="POST">
+            <form action="{{ route('jeniskasus') }}" method="POST">
                 @csrf
                 <div class="modal-body">
                     <div class="form-group">
