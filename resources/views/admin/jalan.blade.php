15d99fb3c43eef58a3d56e4592c7046b4f1eac5f
Fahrezy22 <fahriza2222@gmail.com>
2021-07-09 19:37:42 +0800

    create notification alert delete data

--- resources/views/admin/jalan.blade.php
+++ resources/views/admin/jalan.blade.php
@@ -134,4 +134,27 @@
     </div>
 </form>
 @endforeach
+
+{{-- modal delete --}}
+@foreach ($data as $d)
+    <div class="modal fade" id="delete-{{$d->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Hapus data</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <h3>Apakah anda yakin ingin menghapus data <strong>{{ $d->nama_jalan }}</strong> ???</h3>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">BATAL</button>
+                <a href="/admin/jalan/delete/{{ $d->id }}" class="btn btn-primary">SIMPAN</a>
+            </div>
+            </div>
+        </div>
+    </div>
+@endforeach
 @endsection
