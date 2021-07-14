23576697fee307f512772227a9d748d79bb7d415
Fahrezy22 <fahriza2222@gmail.com>
2021-07-15 05:18:51 +0800

    create migration table for contact us

---
+++ database/migrations/2021_07_15_051439_create_contact_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact_us', function (Blueprint $table) {
+            $table->id();
+            $table->string('alamat');
+            $table->string('telepon');
+            $table->string('email');
+            $table->date('created_at');
+            $table->date('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact_us');
+    }
+}
